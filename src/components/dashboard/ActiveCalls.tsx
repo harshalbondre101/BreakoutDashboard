d682141632d67a9b8e76238e8e5da76ebfb9fdf9
Harshal Bondre <harshalbondre101@gmail.com>
2025-10-28 06:38:18 +0000

    if theres no data to show or any fetch error or api didnt return the dat

--- src/components/dashboard/ActiveCalls.tsx
+++ src/components/dashboard/ActiveCalls.tsx
@@ -21,12 +21,12 @@ export const ActiveCalls = ({ activeCalls, callsLoading, callsError }: { activeC
       );
     }
 
-    if (callsError) {
+    if (callsError || activeCalls.length === 0) {
       return (
-        <div className="max-h-96 flex items-center justify-center bg-gray-50 rounded-lg">
-          <div className="text-red-500 text-center">
-            <p>Failed to load active calls.</p>
-            <p className="text-sm">{callsError}</p>
+        <div className="max-h-96 h-60 flex items-center justify-center bg-gray-50 rounded-lg">
+          <div className="text-gray-500 text-center">
+            <p>No Data to Show</p>
+             {callsError && <p className="text-sm text-red-500">{callsError}</p>}
           </div>
         </div>
       );
