3675461bf9a161a492e44e9f9f9539368b6db62a
Harshal Bondre <harshalbondre101@gmail.com>
2025-10-29 11:20:11 +0000

    move recent booking in dashboard to down so that charts section can take

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -30,9 +30,10 @@ function DashboardContent() {
     <div className="space-y-6">
       <KpiGrid kpiMetrics={kpiMetrics} kpiLoading={kpiLoading} kpiError={kpiError} />
 
+      <AnalyticsOverview />
+      
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2 space-y-6">
-           <AnalyticsOverview />
            <ActiveCalls 
             activeCalls={activeCalls} 
             callsLoading={callsLoading} 
