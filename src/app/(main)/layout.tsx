4851ec5a4ca7569bce7e789e469ab82369dee0b5
Harshal Bondre <harshalbondre101@gmail.com>
2025-11-03 10:25:31 +0000

    try this one (_for element div_)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -99,7 +99,7 @@ export default function MainLayout({
                   sidebarOpen ? 'ml-64' : 'ml-20'
                 }`}
               >
-                <div className="p-8">
+                <div className="p-6">
                   {children}
                 </div>
               </main>
