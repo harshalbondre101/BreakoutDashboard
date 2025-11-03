d976762ba07ee91c3d96f3cf8ce138178c4660ac
Harshal Bondre <harshalbondre101@gmail.com>
2025-11-03 10:42:57 +0000

    its not using device whole width from right. theres visible space which

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -99,7 +99,7 @@ export default function MainLayout({
                   sidebarOpen ? 'ml-64' : 'ml-20'
                 }`}
               >
-                <div className="py-6 px-6">
+                <div className="py-6 px-8">
                   {children}
                 </div>
               </main>
